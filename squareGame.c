8add92a6eca50d83c2504e2e26b0bd05684375cf
“Liangjie <liangjieshen@cpp.edu>
2022-03-09 14:34:22 -0800

    correct if and while loop bugs and finish up code

--- squareGame.c
+++ squareGame.c
@@ -26,10 +26,10 @@ int main()
     printf("Welcome to the Lo Shu Magic Square!\n");
 
     // loop until the magic square is valid
-    bool valid = 0;
+    int valid = 0;
     int squaresGenerated = 1;
 
-    while(valid==1){
+    while(valid != 1){
         // shuffle the array with number 1-9
         for(int i = 0; i < 9; i++){
             int index = rand() % 9;
@@ -51,41 +51,31 @@ int main()
 
         // check if it is a Lo Shu Magic Square
         int sum = 15; // sum for each row, column or diagonally
-        
-        valid = 1;
-        // check sum of each row first
-        for(int row = 0; row < arraySize; row++){
-            if(magicSquare[row][0]+magicSquare[row][1]+magicSquare[row][2] != 15){
-                valid = 0;
-                break;
-            }
-        }
+       
+        if(
+            // check the sum of each row
+           magicSquare[0][0]+magicSquare[0][1]+magicSquare[0][2] == 15 &&
+           magicSquare[1][0]+magicSquare[1][1]+magicSquare[1][2] == 15 &&
+           magicSquare[2][0]+magicSquare[2][1]+magicSquare[2][2] == 15 &&
 
-        // check sum of each column
-        for(int col = 0; col < arraySize; col++){
-            if(magicSquare[0][col]+magicSquare[1][col]+magicSquare[2][col] != 15){
-                valid = 0;
-                break;
-            }
-        }
+            // check the sum of each column
+           magicSquare[0][0]+magicSquare[1][0]+magicSquare[2][0] == 15 &&
+           magicSquare[0][1]+magicSquare[1][1]+magicSquare[2][1] == 15 &&
+           magicSquare[0][2]+magicSquare[1][2]+magicSquare[2][2] == 15 &&
 
-        // check sum of diagonals
-        if(magicSquare[0][0]+magicSquare[1][1]+magicSquare[2][2]!=15)
-            valid = 0;
-        
-
-        else if(magicSquare[0][2]+magicSquare[1][1]+magicSquare[2][0]!=15)
-            valid = 0;
-        
+            // check the diagonal sum
+           magicSquare[0][0]+magicSquare[1][1]+magicSquare[2][2] == 15 &&
+           magicSquare[0][2]+magicSquare[1][1]+magicSquare[2][0] ==15 )
+                valid = 1;
+            
         else
-            continue;
-
-        if(valid == 0)
             squaresGenerated++;
+
          
     }
 
-    printf("\n\nThe total number of squares generated and tested before success: %d",squaresGenerated);
+    // The total number of squares generated and tested before success, the count.
+    printf("The total number of squares generated and tested before success: %d",squaresGenerated);
 
     // print randomnized array
     printf("\nHere is a valid randomnized Lo Shu Magic Square:\n");
