0c60ed9f9e47ff58c42decc11af3e4b2c75d90f3
“Liangjie <liangjieshen@cpp.edu>
2022-03-09 12:34:32 -0800

    updated valid game while loop

--- squareGame.c
+++ squareGame.c
@@ -3,6 +3,7 @@
 #include<stdio.h> // input/output library
 #include <stdlib.h> // random no. library
 #include <time.h> // get time variable
+#include <stdbool.h> // boolean library
 
 // declaration of global constant
 const int arraySize = 3; // size for 2-d array
@@ -24,7 +25,11 @@ int main()
     // welcome message
     printf("Welcome to the Lo Shu Magic Square!\n");
 
-    while(true){
+    // loop until the magic square is valid
+    bool valid = 0;
+    int squaresGenerated = 1;
+
+    while(valid==1){
         // shuffle the array with number 1-9
         for(int i = 0; i < 9; i++){
             int index = rand() % 9;
@@ -45,15 +50,45 @@ int main()
         }
 
         // check if it is a Lo Shu Magic Square
-        for(int i = 0; i < arraySize; i++) {
-            for(int j = 0 ; j < arraySize ; j++) {
-                if(magicSquare[i][j])
+        int sum = 15; // sum for each row, column or diagonally
+        
+        valid = 1;
+        // check sum of each row first
+        for(int row = 0; row < arraySize; row++){
+            if(magicSquare[row][0]+magicSquare[row][1]+magicSquare[row][2] != 15){
+                valid = 0;
+                break;
             }
         }
 
+        // check sum of each column
+        for(int col = 0; col < arraySize; col++){
+            if(magicSquare[0][col]+magicSquare[1][col]+magicSquare[2][col] != 15){
+                valid = 0;
+                break;
+            }
+        }
+
+        // check sum of diagonals
+        if(magicSquare[0][0]+magicSquare[1][1]+magicSquare[2][2]!=15)
+            valid = 0;
+        
+
+        else if(magicSquare[0][2]+magicSquare[1][1]+magicSquare[2][0]!=15)
+            valid = 0;
+        
+        else
+            continue;
+
+        if(valid == 0)
+            squaresGenerated++;
+         
     }
+
+    printf("\n\nThe total number of squares generated and tested before success: %d",squaresGenerated);
+
     // print randomnized array
-    printf("\nHere is a randomnized magic square:\n");
+    printf("\nHere is a valid randomnized Lo Shu Magic Square:\n");
     for(int i = 0; i < arraySize; i++) {
       for(int j = 0 ; j < arraySize ; j++) {
           printf("%d ",magicSquare[i][j]);
