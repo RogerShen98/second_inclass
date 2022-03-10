e83514caa1532e1e3adfb65853481e157e6e3976
Liangjie Shen <98449194+RogerShen98@users.noreply.github.com>
2022-03-09 16:56:33 -0800

    Update employeeMain.c

--- employeeMain.c
+++ employeeMain.c
@@ -1,65 +1,34 @@
 //Compile using the following command: gcc employeeMain.c employeeTable.c employeeOne.c 
-
- 
-
 //gcc employeeMain.c employeeTable.c employeeTwo.c 
 
+#include <string.h> 
+#include <stdlib.h> 
+#include "employee.h" 
+
+int main(void){ 
+    //defined in employeeSearchOne.c 
+    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind); 
+    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind);
  
+    //defined in employeeTable.c 
+    extern Employee EmployeeTable[];     
+    extern const int EmployeeTableEntries;      
 
-#include <string.h> 
+    PtrToEmployee matchPtr;  //Declaration 
+    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045); 
 
-#include <stdlib.h> 
+    //Example not found 
+    if (matchPtr != NULL) 
+        printf("Employee ID 1045 is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee ID 1045 is NOT found in the record\n"); 
 
-#include "employee.h" 
-
-int main(void){ 
-
-    //defined in employeeSearchOne.c 
-
-    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind); 
-
-    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind); 
-
- 
-
-    //defined in employeeTable.c 
-
-    extern Employee EmployeeTable[];     
-
-    extern const int EmployeeTableEntries;      
-
- 
-
-    PtrToEmployee matchPtr;  //Declaration 
-
-    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045); 
-
- 
-
-    //Example not found 
-
-    if (matchPtr != NULL) 
-
-        printf("Employee ID 1045 is in record %d\n", matchPtr - EmployeeTable); 
-
-    else 
-
-        printf("Employee ID is NOT found in the record\n"); 
-
- 
-
-    //Example found 
-
-    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat"); 
-
-    if (matchPtr != NULL) 
-
-        printf("Employee Tony Bobcat is in record %d\n", matchPtr - EmployeeTable); 
-
-    else 
-
-        printf("Employee Tony Bobcat is NOT found in the record\n"); 
-
-     
-
-    return EXIT_SUCCESS; 
+    //Example found 
+    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat"); 
+    if (matchPtr != NULL) 
+        printf("Employee Tony Bobcat is in record %ld\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee Tony Bobcat is NOT found in the record\n"); 
+   
+    return EXIT_SUCCESS; 
+} 
